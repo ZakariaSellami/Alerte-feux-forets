6a62e592d551f810e9a19b9d7fc72ba10f9435a6
zakariasellami <zakaria01sellami@gmail.com>
2024-03-22 00:18:09 +0100

    first commit

--- server.js
+++ server.js
@@ -31,20 +31,20 @@ app.get("/sensors", async (req, res) => {
 
 
 //updating database esp 8266/32
-// app.post('/edit-sensor', async (req, res) => {
-//     const { sensorID, temp, fire, smoke } = req.body;
+app.post('/edit-sensor', async (req, res) => {
+    const { sensorID, temp, fire, smoke } = req.body;
 
-//     try {
-//         await SensorModel.updateOne({ sensorID }, { temp },{ fire }, { smoke });
+    try {
+        await SensorModel.updateOne({ sensorID }, { temp },{ fire }, { smoke });
 
-//         // console.log(`Sensor ID: ${sensorID}, Fuite de gaz: ${étatGaz}`);
-//         res.json({ message: 'Data received and database updated successfully' });
-//     }
-//     catch (error) {
-//         console.error('Error updating database:', error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
+        // console.log(`Sensor ID: ${sensorID}, Fuite de gaz: ${étatGaz}`);
+        res.json({ message: 'Data received and database updated successfully' });
+    }
+    catch (error) {
+        console.error('Error updating database:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 
 
